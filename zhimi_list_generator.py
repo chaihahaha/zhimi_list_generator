c5ffa2e1495af8eb926541b929f86b0bfc4b8c69
chaihahaha <836275709@qq.com>
2018-12-31 20:19:46 +0800

    Update zhimi_list_generator.py

--- zhimi_list_generator.py
+++ zhimi_list_generator.py
@@ -6,7 +6,8 @@ words = re.findall('\w+', text.lower())
 uniq_words = set(words)
 w = open(sys.argv[2],'w')
 for i in uniq_words:
-	w.write(i)
-	w.write('\n')
+	if len(i) > 3 and not any(char.isdigit() for char in i):
+		w.write(i)
+		w.write('\n')
 f.close()
 w.close()
