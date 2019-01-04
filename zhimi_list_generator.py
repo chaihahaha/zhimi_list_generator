aa91cc1c27bbf4ab8b15318391a0c37019303ecd
chaihahaha <836275709@qq.com>
2019-01-04 18:49:43 +0800

    Update zhimi_list_generator.py

--- zhimi_list_generator.py
+++ zhimi_list_generator.py
@@ -6,7 +6,7 @@ words = re.findall('\w+', text.lower())
 uniq_words = set(words)
 w = open(sys.argv[2],'w')
 for i in uniq_words:
-	if len(i) > 3 and all((char>='a' and char<='z') or (char>='A' and char<='Z') for char in i):
+	if len(i) > 3 and all((char>='a' and char<='z') or (char>='A' and char<='Z') or (char>='Ï' and char<='œ')  for char in i):
 		w.write(i)
 		w.write('\n')
 f.close()
