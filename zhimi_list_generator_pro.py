b6b0561cbdb8ced20fae306639d786bf09fc1d59
柴士童 <836275709@qq.com>
2019-05-09 17:47:56 +0800

    Update zhimi_list_generator_pro.py

--- zhimi_list_generator_pro.py
+++ zhimi_list_generator_pro.py
@@ -6,28 +6,18 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.probability import FreqDist
 from nltk import RegexpTokenizer
 toknizer = RegexpTokenizer(r'''\w'|\w+|[^\w\s]''')
-def get_unique_set(path, coding, lang):
-    if path:
-        f = open(path,encoding=coding)
-        text = f.read()
-        f.close()
-        words = toknizer.tokenize(text)
-        words = [WordNetLemmatizer().lemmatize(WordNetLemmatizer().lemmatize(word,'v'),'n') for word in words]
-    else:
-        return set()
-def get_unique_words(path,exclude, coding, lang):
+def get_unique_words(path,exclude, coding):
     f = open(path,encoding=coding)
     text = f.read()
     f.close()
     words = toknizer.tokenize(text)
     words = [WordNetLemmatizer().lemmatize(WordNetLemmatizer().lemmatize(word,'v'),'n') for word in words]
-    uniq_words = list(set(words)-get_unique_set(exclude, 'utf8', lang))
     fdist = FreqDist(words)
     tops = fdist.most_common(len(fdist))
     return tops
 
     
-tops = get_unique_words(sys.argv[1],sys.argv[3] if len(sys.argv)==4 else "",'utf8','french')
+tops = get_unique_words(sys.argv[1],sys.argv[3] if len(sys.argv)==4 else "",'utf8')
 
 topf = open(sys.argv[2],'w',encoding='utf8')
 for j in tops:
